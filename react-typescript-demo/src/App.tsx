03844bd3d13d0eee487f96e195087be1e3d4efc7
AdiletZhusupov <adilet.zhusupov@gmail.com>
2022-12-10 18:47:16 +0300

    useRef, Component as prop, generics, class components, restricting with never

--- react-typescript-demo/src/App.tsx
+++ react-typescript-demo/src/App.tsx
@@ -23,6 +23,15 @@ import "./App.css";
 // import { UserContextProvider } from "./components/context/UserContext";
 // import { User } from "./components/context/User";
 
+//Tutorials 16-20
+// import { DomRef } from "./components/refs/DomRef";
+// import { MutableRef } from "./components/refs/MutableRef";
+// import { Counter } from "./components/class/Counter";
+// import { Private } from "./components/auth/Private";
+// import { Profile } from "./components/auth/Profile";
+// import { List } from "./components/generics/List";
+// import { RandomNumber } from "./components/restriction/RandomNumber";
+
 function App() {
   /* Tutorials 1-5
   const personName = {
@@ -66,6 +75,16 @@ function App() {
       <UserContextProvider>
         <User/>
       </UserContextProvider>*/}
+
+      {/* Tutorials 16-20
+      <DomRef/>
+      <MutableRef/>
+      <Counter message="The count value is"/>
+      <Private isLoggedIn={false} Component={Profile}/>
+      <List items={['Batman', 'Superman', 'Wonder Woman']} onClick={(item) => console.log(item)}/>
+      <List items={[1, 2, 3]} onClick={(item) => console.log(item)}/>
+      <List items={[{id: 1, first: 'Bruce', last: 'Wayne'},{id: 2, first: 'Clark', last: 'Kent'},{id: 3, first: 'Princess', last: 'Diana'}]} onClick={(item) => console.log(item)}/>
+      <RandomNumber value={10} isPositive/>*/}
     </div>
   );
 }
